9d11104194e18cd5c24fd149eb033ee8d8d4729f
Asitha Dissanayaka <adissanayaka470@gmail.com>
2026-07-09 13:11:20 +0530

    Change MongoDB connection string to placeholder
    Updated MongoDB connection string to use a placeholder.

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://aadorg:AADORG1234@cluster0.9eabafx.mongodb.net/food-del"
+      "mongodb+srv://url***"
     )
     .then(() => console.log("DB Connected"));
 };
